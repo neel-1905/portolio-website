ca16985fa6e492c3401bd38262c2975ced66d07c
neel-1905 <neelnarwadkar@gmail.com>
2024-07-13 12:51:38 +0530

    seo changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,14 +1,14 @@
-import SEO from "../SEO";
+// import SEO from "../SEO";
 import AboutSection from "../about/AboutSection";
 import ContactSection from "../contact/ContactSection";
 import HeroSection from "../hero/HeroSection";
 import ProjectsSection from "../projects/ProjectsSection";
-import logo from "../../assets/images/N.png";
+// import logo from "../../assets/images/N.png";
 
 const HomePage = () => {
   return (
     <>
-      <SEO
+      {/* <SEO
         title={"Portfolio | Home"}
         description={
           "Welcome to Neel's Portfolio Website. Neel is a full stack web developer. He specializes in building accessible and pixel perfect UI for websites and web applications as well as REST APIs."
@@ -19,7 +19,7 @@ const HomePage = () => {
         url={`https://neel-narwadkar-portfolio.vercel.app/`}
         image={logo}
         author="Neel Narwadkar"
-      />
+      /> */}
       <HeroSection />
       <ProjectsSection />
       <AboutSection />
